08a91517f592fff9302527b53910ddb70df448e5
vperkonigg <153551249+vperkonigg@users.noreply.github.com>
2023-12-11 19:27:10 +0000

    minimal setup

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,9 @@ export const app = express();
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
 
 app.use(logger("dev"));
 
-app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", index);
 
 app.use(errorNotFoundHandler);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import * as controller from "../controllers/index";
 
 export const index = Router();
 
-index.get("/", controller.index);
+index.get("/", () => async (req: Request, res: Response): Promise<void> => {
+    res.send();
+});
